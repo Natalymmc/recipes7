65cfb20a11d172d542cecb925266d03f7365252a
Natalymmc <ulickaanatala6@gmail.com>
2025-02-19 03:30:43 +0300

    Initial commit: changed get

--- src/main/java/com/example/recipes/controller/RecipeController.java
+++ src/main/java/com/example/recipes/controller/RecipeController.java
@@ -3,8 +3,8 @@ package com.example.recipes.controller;
 import com.example.recipes.model.Recipe;
 import com.example.recipes.service.RecipeService;
 import java.util.List;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,25 +24,13 @@ public class RecipeController {
 
 
     @GetMapping
-    public ResponseEntity<List<Recipe>> getRecipes(@RequestParam(value = "title",
-            required = false) String title) {
-        List<Recipe> recipes;
-        if (title != null && !title.isEmpty()) {
-            recipes = recipeService.getRecipesByTitle(title);
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-        return ResponseEntity.ok(recipes);
+    public List<Recipe> getRecipes(@RequestParam(required = false) String title) {
+        return recipeService.getRecipesByTitle(title);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Recipe> getRecipeById(@PathVariable Long id) {
-        Recipe recipe = recipeService.getRecipeById(id);
-        if (recipe != null) {
-            return ResponseEntity.ok(recipe);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public Recipe getRecipeById(@PathVariable Long id) {
+        return recipeService.getRecipeById(id);
     }
 }
 
